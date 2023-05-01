979242f16b3739142a50a9c1b52631982e88bfdb
rafpabc <rafpab.analyst@gmail.com>
2023-04-30 20:04:46 -0500

    ready for deployment

--- server.R
+++ server.R
@@ -1,66 +1,9 @@
 library(tidyverse)
 library(shiny)
-library(ggrepel)
 #library(shinythemes)
 options(scipen = 99)
 
-backgroundImageCSS <- "height:100vh;background-size: cover;background-image: url('%s');"
-addResourcePath(prefix = 'www', directoryPath = 'C:/Users/usuario/Documents/GitHub/inflation_calculator/www')
-
-
-ui <- fluidPage(
-  tags$head(
-  tags$style(HTML("
-                #past_year 
-                  {background-color: #d6ffa1;color:black}
-                #to_year 
-                  {background-color: #95d447;color:black}
-                #convert_value 
-                  {background-color: #5b9b07;color:black}
-                #submit_button
-                  {background-color: #f5c116;width: 50%;display:inline-block;margin:0px 20%;font-family:Helvetica;font-weight:bold;border-radius: 20px}
-                "
-  )
-  )),
-  titlePanel(div(paste("Inflation Calculator ",year(today()), sep =""),style="font-family:Helvetica;font-weight:bold;color:white;padding-bottom:20px")),
-  fluidRow(
-    column(width=3,
-    div(selectInput(inputId = "past_year",label = "From year:",
-                choices = c("2023","2022","2021","2020","2019","2018","2017","2016","2015","2014",
-                            "2013","2012","2011","2010","2009","2008","2007","2006","2005",
-                            "2004","2003","2002","2001","2000"),selectize = FALSE),style="color:white"),
-    div(selectInput("to_year",label = "To year:",
-                choices = c("2023","2022","2021","2020","2019","2018","2017","2016","2015","2014",
-                            "2013","2012","2011","2010","2009","2008","2007","2006","2005",
-                            "2004","2003","2002","2001","2000"),selectize = FALSE),style="color:white"),
-    div(numericInput("convert_value",label = "Money to convert to present value:",
-                 value=0),style="color:white"),
-    actionButton("submit_button","SUBMIT")
-    ),
-    column(width=9,
-           div(plotOutput("line_plot",hover=hoverOpts("hover_money")),style="padding-right:6%"),
-           uiOutput("hover_info"))
-  ),
-  fluidRow(
-    div(column(width = 3),
-  column(width = 3,
-         textOutput("subtitle_1"),
-         textOutput("present_value")
-         ),
-  column(width = 3,
-         textOutput("subtitle_2"),
-         textOutput("equivalent_value")
-         ),
-  column(width = 3,
-         textOutput("subtitle_3"),
-         textOutput("perc_lost")
-         ),style="padding-right:6%")
-),
-style=sprintf(backgroundImageCSS, "www/backgr_calc.png"),
-
-)
-
-server <- function(input, output) {
+function(input, output) {
   year_inflation <- as.data.frame(cbind(c(2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,
                             2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,
                             2000),
@@ -158,5 +101,3 @@ server <- function(input, output) {
   )
   
 }
-
-shinyApp(ui, server)
